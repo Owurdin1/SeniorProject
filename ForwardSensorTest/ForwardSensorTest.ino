b4c812ec56b3ad5c66223b0f217c64a4f73849d2
owurdin1 <owurdin1@gmail.com>
2013-02-15 21:25:52 -0800

    This version has been changed to allow for an enumeration for drone states, exanded rangeStop value to 15 inches and re-formatted the forwardState command string to send proper values to the drone.

--- ForwardSensorTest/ForwardSensorTest.ino
+++ ForwardSensorTest/ForwardSensorTest.ino
@@ -47,6 +47,10 @@ unsigned char ssid_len;
 unsigned char security_passphrase_len;
 //---------------------------------------------------------------------------
 
+//===========================================================================
+int testing = 0;
+//===========================================================================
+
 // Variables for drone states
 char data[100];
 char yj_mac[17];
@@ -56,15 +60,27 @@ unsigned long time;
 unsigned long flightTime;
 unsigned long totalTime;
 int droneState = 0;	// Drone state variable
-int rangeStop = 7;	// Range value to stop drone
+int rangeStop = 15;	// Range value to stop drone
 int leftSensor[3];	// Array containing 3 left sensor reads
 int rightSensor[3];	// Array containing 3 right sensor reads
 int verticalSensor[3];	// Array containing 3 vertical sensor reads
 int leftRange = 0;	// largest value of leftSensor[3]
 int rightRange = 0;	// largest value of rightSensor[3]
 int verticalRange = 0;	// largest value of verticalSensor[3]
-int forwardSpeed = -0.2; // forward speed state
+int forwardSpeed = 1; // forward speed state
 
+// Enumerator for drone States
+enum droneFlightState
+{
+	LANDED,
+	TAKEOFF,
+	HOVERING,
+	EMERGENCYLAND,
+	FORWARDFLIGHT,
+	TURN,
+	FLATTRIMMING,
+	RESETWATCHDOG
+};
 
 // flightRange Test demo variables
 int verticalTrigger = 0;
@@ -88,7 +104,12 @@ void initialState()
 	keepConn = time;
 	totalTime = time;
         sprintf(data,"AT*PCMD=%d,0,0,0,0,0\rAT*REF=%d,290717696\r",1,1);
+	Serial.println("initialState first command: ");
+	Serial.println(data);
+
 	sprintf(data, "AT*FTRIM=%d\r", seq++);
+	Serial.println("flatTrim second command: ");
+	Serial.println(data);
 
 	delay(200);
 //aeq++;
@@ -97,40 +118,53 @@ void initialState()
 void landingState()
 {
         //Serial.println("landingState send land command");
-        droneState = 0;
+        //droneState = 0;
         sprintf(data,"AT*PCMD=%d,0,0,0,0,0\rAT*REF=%d,290717696\r",seq++,seq++);  //,aeq++); //seq++);
+	Serial.println("landingState: ");
+	Serial.println(data);
 }
 
 void takeOffState()
 {
         //Serial.println("takeOffState, tell drone to take off");
-        droneState = 1;
+        //droneState = 1;
         sprintf(data,"AT*PCMD=%d,0,0,0,0,0\rAT*REF=%d,290718208\r",seq++,seq++);  //,aeq++); //seq++);
+	Serial.println("takeOffState: ");
+	Serial.println(data);
 }
 
 void hoverState()
 {
         //Serial.println("hoverState, hovring with no directions");
-        droneState = 2;
+        //droneState = 2;
         sprintf(data,"AT*PCMD=%d,0,0,0,0,0\r",seq++);
+	Serial.println("hoverState: ");
+	Serial.println(data);
 //aeq++;
 }
 
 void emergencyLandState()
 {
         //Serial.println("landingState send land command");
-        droneState = 3;
+        //droneState = 3;
 	//sprintf(data,"AT*REF=%d,290717696\r", seq++);
         sprintf(data,"AT*PCMD=%d,0,0,0,0,0\rAT*REF=%d,290717696\r",seq++, seq++);  //,aeq++); //seq++);
+	Serial.println("emergencyLandState: ");
+	Serial.println(data);
 }
 
 void forwardState()
 {
 	//Serial.println("forwardState, moving forward");
-	droneState = 4;
+	//droneState = 4;
 
 	// Entered a 1 for the second arg in the command to enable reading other values for control
-	sprintf(data,"AT*PCMD=%d,1,0,%d,0,0\r",seq++,forwardSpeed);
+	sprintf(data, "AT*PCMD=%d,1,0,-.%d,0,0\r", seq++, forwardSpeed);
+	Serial.println("forwardState (data): ");
+	Serial.println(data);
+	Serial.println("");
+	Serial.print("forwardSpeedValue: ");
+	Serial.println(forwardSpeed);
         //sprintf(data,"AT*PCMD=%d,1,0,forwardSpeed,0,0\r",seq++);
         //sprintf(data,"AT*PCMD=%d,0,0,forwardSpeed,0,0\r",seq++);
 }
@@ -144,24 +178,21 @@ void keepConnection()
 	{
 		  keepConn = millis();
 
-		  Serial.print("Keeping drone connection alive, droneState = ");
-		  Serial.println(droneState);
-
 		  switch(droneState)
 		  {
-			case 0:
+			case LANDED:
 				landingState();
 				break;
-			case 1:
+			case TAKEOFF:
 				takeOffState();
 				break;
-			case 2:
+			case HOVERING:
 				hoverState();
 				break;
-			case 3:
+			case EMERGENCYLAND:
 				emergencyLandState();
 				break;
-			case 4:
+			case FORWARDFLIGHT:
 				forwardState();
 				break;
 		  }
@@ -198,7 +229,7 @@ void sensorCompare()
 	verticalRange = verticalRead;
 
 	// DebugPrintFunction
-	debugPrint();
+	//debugPrint();
 }
 
 void rangeCheck()
@@ -220,142 +251,93 @@ void rangeCheck()
 	}
 }
 
+int goForward()
+{
+	//debugPrint();
+
+	if (leftRange > rangeStop && rightRange > rangeStop)
+	{
+		Serial.println("goForward returning a #1!");
+		return 1;
+	}
+
+	Serial.println("goForward returning a 0!");
+	return 0;
+}
+
 void forwardSensorTest()
 {
-        if (time == 0)
-        {
-                initialState();
-        }
+	if (time == 0)
+	{
+		initialState();
+	}
+
+	// populate range arrays and get range values
+	if (millis() - time > 100)
+	{
+		rangeClass.GetRanges();
+		rangeCheck();
+		time = millis();
+	}
 
 	if (rangeTimer >= 3)
 	{
 		rangeTimer = 0;
 
-		switch(droneState)
+		switch (droneState)
 		{
-			case 0:
-				if (millis() - flightTime > 5000)
-				{
-					takeOffState();
-					flightTime = millis();
-				}
-				else
-				{
-					keepConnection();
-				}
-
-				break;
-			case 1:
-				if (millis() - flightTime > 5000)
-				{
-					hoverState();
-					flightTime = millis();
-				}
-				else
-				{
-					keepConnection();
-				}
-
-				break;
-			case 2:
-				forwardState();
-
-				flightTime = millis();
-
-				Serial.println("Flying forward now");
-
-				break;
-
-			case 4:
-				keepConnection();
-/*
-				if (millis() - flightTime > 3000)
-				{
-					landingState();
-					Serial.println("Forward flight for 3 seconds complete");
-
-					while (1)
-					{
-						keepConnection();
-					}
-				}
-				else
-				{
-					keepConnection();
-				}
-*/
-				break;
-
-			default:
-				keepConnection();
-				break;			
-		}
-	}
-
-	if (millis() - time > 100)
-	{
-		 // Range finders funciton
-		rangeClass.GetRanges();
-		rangeCheck();
-		time = millis();
-
-		//if (rangeClass.sensors[2] < 10)
-		if ((leftRange < rangeStop) && (leftRange != 0) || (rightRange < rangeStop) && (rightRange != 0))
-		{
-			Serial.println("RangeViolation!!!! Land");
-
-			rangeViolation++;
-			Serial.println("Switch to hover!");
-			hoverState();
-			time = millis();
-			while (millis() - time < 2000)
+			case LANDED:
 			{
-				keepConnection();
-				Serial.print("DroneState; ");
-				Serial.print(droneState);
-				Serial.println(";");
+				if (millis() - flightTime > 5000)
+				{
+					droneState = TAKEOFF;
+					flightTime = millis();
+				}
+
+				break;
 			}
-
-			if (rangeViolation == 3)
+			case TAKEOFF:
 			{
-				hoverState();
-
-				while (millis() - time < 2000)
+				if (millis() - flightTime > 5000)
 				{
-					keepConnection();
+					droneState = HOVERING;
+					flightTime = millis();
 				}
 
-				landingState();
-
-				while (1)
+				break;
+			}
+			case HOVERING:
+			{
+				if (millis() - flightTime > 5000)
 				{
-					keepConnection();
+					droneState = FORWARDFLIGHT;
+					flightTime = millis();
+				}
+
+				break;
+			}
+			case FORWARDFLIGHT:
+			{
+				if (goForward())
+				{
+					Serial.println("Flying forward!!!!");
+					droneState = FORWARDFLIGHT;
+				}
+				else
+				{
+					droneState = HOVERING;
 				}
 			}
 		}
 	}
 
-	if (millis() - totalTime > 60000)
+	if (millis() - totalTime > 45000)
 	{
-
-		Serial.println("Flight done LAND and STAY DOWN!");
-		flightTime = millis();
-		hoverState();
-
-		while (millis() - flightTime < 2000)
-		{
-			keepConnection();
-		}
-
-		landingState();
-
-		while(1)
-		{
-			keepConnection();
-		}
+		droneState = EMERGENCYLAND;
 	}
 
-	debugPrint();
+	keepConnection();
+
 }
 
 void debugPrint()
@@ -381,19 +363,29 @@ void setup()
         Serial.println("");
         Serial.println("Initialize Connection");
         
-	WiFi.init();
-        Serial.println("Initialized");
+	if (!testing)
+	{
+		WiFi.init();
+		Serial.println("Initialized");
         
-        unsigned char *my_mac = zg_get_mac();
-        sprintf(yj_mac,"%.2X:%.2X:%.2X:%.2X:%.2X:%.2X",my_mac[0],my_mac[1],my_mac[2],my_mac[3],my_mac[4],my_mac[5]);
-        
-        Serial.print("MAC Address: ");
-        Serial.println(yj_mac);
+		unsigned char *my_mac = zg_get_mac();
+		sprintf(yj_mac,"%.2X:%.2X:%.2X:%.2X:%.2X:%.2X",my_mac[0],my_mac[1],my_mac[2],my_mac[3],my_mac[4],my_mac[5]);
+		
+		Serial.print("MAC Address: ");
+		Serial.println(yj_mac);
+	}
+	else
+	{
+		Serial.println("Testing program");
+	}
 }
 
 void loop()
 {
-	WiFi.run();
+	if (!testing)
+	{
+		WiFi.run();
+	}
 	
         // Function to begin forward flight test
         forwardSensorTest();
