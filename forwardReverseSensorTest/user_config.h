c8b6220dd6219b85cb46654fca4fbbb4e5f4adb1
owurdin1 <owurdin1@gmail.com>
2013-02-16 20:11:49 -0800

    forwardReverseSensorTest is a copy of ForwardSensorTest. This test will use a reverse command after the forward sensors RangeStop value has been breached. This should prevent drone from running into walls.

---
+++ forwardReverseSensorTest/user_config.h
@@ -0,0 +1,12 @@
+#define PRINT_DEBUG    1
+#define WIFI_DISABLE   1
+
+#define DRONE_NAME     "ardrone_277921"
+
+#define YAW_RATE        5
+#define THROTTLE_RATE   1500
+#define PITCH_ROLL_RATE 3
+
+#define ALTITUDE_MAX     5000
+
+#define FLY_OUTDOOR      "FALSE"
