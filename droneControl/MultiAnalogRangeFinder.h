1cfb4654ae26ed8e039133067d68f611b42859d2
owurdin1 <owurdin1@gmail.com>
2013-02-25 12:33:52 -0800

    Created drone control sketch which will be entire drone control state machine to fly drone. This will be used on the board to control input and output for the Simulator to run and simulate what the drone is doing.

---
+++ droneControl/MultiAnalogRangeFinder.h
@@ -0,0 +1,47 @@
+// Header file for MultiAnalogRangeFinderTest
+// using chain read function
+// Date: 20121214
+
+#ifndef MULTI_ANALOG_RANGE_FINDER
+#define MULTI_ANALOG_RANGE_FINDER
+
+// Arduino libraries, needed to use arduino hardware functions
+#include <Arduino.h>
+
+// Define analog pins for range finders
+#define rangePinLeft A0
+#define rangePinRight A1
+#define rangePinVertical A2
+
+// Define pins for control of sensors
+#define bwPin 6
+#define rxPin 7
+
+// Define values for conversions in millivolts
+#define voltsPerInch 6.25 // 9.57
+#define voltsPerUnit 3.2 //4.9
+
+enum rangeFinders
+{
+      left,
+      right,
+      vertical
+};
+
+// Class for reading the range finders and setting a
+// public container for easy access
+class MultiAnalogRange
+{
+  private:
+      // Private functions
+      void ConvertRangeValues();
+      
+  public:
+      // Public Functions
+      void GetRanges();
+      
+      // Global Private Variables
+      // public container for easy access to find range values
+      long sensors[3];  // sensor values, left = 0, right = 1, vertical = 2  
+};
+#endif
---
+++ droneControl/droneControl.ino
@@ -0,0 +1,446 @@
+/*
+ * UDP endpoint
+ *
+ * A simple UDP endpoint example using the WiShield 1.0
+ *
+ * NOTE: You must edit the uip-conf.h file in the WiShield library directory
+ * as follows (at about line 110):
+ *         #define UIP_CONF_UDP             1
+ *
+ * Change this back to 0 for compiling the other examples.
+ *
+ */
+
+#include <WiShield.h>
+#include "MultiAnalogRangeFinder.h"
+#include "user_config.h"
+
+//#define WIRELESS_MODE_INFRA	1
+#define WIRELESS_MODE_ADHOC	2
+
+// Wireless configuration parameters ----------------------------------------
+unsigned char local_ip[] = {192,168,1,4};	// IP address of WiShield
+unsigned char gateway_ip[] = {192,168,1,1};	// router or gateway IP address
+unsigned char subnet_mask[] = {255,255,255,0};	// subnet mask for the local network
+const prog_char ssid[] PROGMEM = {DRONE_NAME};		// max 32 bytes
+
+unsigned char security_type = 0;	// 0 - open; 1 - WEP; 2 - WPA; 3 - WPA2
+
+// WPA/WPA2 passphrase
+const prog_char security_passphrase[] PROGMEM = {"12345678"};	// max 64 characters
+
+// WEP 128-bit keys
+// sample HEX keys
+prog_uchar wep_keys[] PROGMEM = {	0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d,	// Key 0
+									0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	0x00,	// Key 1
+									0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	0x00,	// Key 2
+									0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	0x00	// Key 3
+								};
+
+
+// setup the wireless mode
+// infrastructure - connect to AP
+// adhoc - connect to another WiFi device
+unsigned char wireless_mode = WIRELESS_MODE_ADHOC;
+
+unsigned char ssid_len;
+unsigned char security_passphrase_len;
+//---------------------------------------------------------------------------
+
+//===========================================================================
+int testing = 1;
+//===========================================================================
+
+// Variables for drone states
+char data[100];
+char yj_mac[17];
+int seq = 1;	// First sequence command counter
+  //int aeq = 1;	// Second sequence command counter
+unsigned long time;
+unsigned long flightTime;
+unsigned long totalTime;
+unsigned long reverseTime;
+int droneState = 0;	// Drone state variable
+int rangeStop = 15;	// Range value to stop drone
+int leftSensor[3];	// Array containing 3 left sensor reads
+int rightSensor[3];	// Array containing 3 right sensor reads
+int verticalSensor[3];	// Array containing 3 vertical sensor reads
+int leftRange = 0;	// largest value of leftSensor[3]
+int rightRange = 0;	// largest value of rightSensor[3]
+int verticalRange = 0;	// largest value of verticalSensor[3]
+int reverseTrigger = 0;	// reverse trigger, set to 1 if in reverseState
+float forwardSpeed = -0.1; // forward speed state
+float reverseSpeed = 0.2;	// reverse speed
+
+// Enumerator for drone States
+enum droneFlightState
+{
+	LANDED,
+	TAKEOFF,
+	HOVERING,
+	EMERGENCYLAND,
+	FORWARDFLIGHT,
+	TURN,
+	FLATTRIMMING,
+	RESETWATCHDOG,
+	REVERSE
+};
+
+// flightRange Test demo variables
+int verticalTrigger = 0;
+int forwardTrigger = 0;
+int rangeViolation = 0;
+
+// Variables for range finders
+MultiAnalogRange rangeClass;
+int rangeTimer = 0;
+
+// Variables for Keep Connection
+unsigned long keepConn;
+
+void initialState()
+{
+        //Serial.println("initialState keep on ground until given command to fly");
+	//Serial.println("sending flat trim command to drone to attempt to prevent flying into walls");
+        time = millis();
+	rangeTimer = time;
+	flightTime = time;
+	keepConn = time;
+	totalTime = time;
+        sprintf(data,"AT*PCMD=%d,0,0,0,0,0\rAT*REF=%d,290717696\r",1,1);
+	Serial.println("initialState command string: ");
+	Serial.println(data);
+
+	reverseTime = 0;
+
+	sprintf(data, "AT*FTRIM=%d\r", seq++);
+	Serial.println("flatTrim command string: ");
+	Serial.println(data);
+
+	delay(200);
+//aeq++;
+}
+
+void landingState()
+{
+        //Serial.println("landingState send land command");
+        //droneState = 0;
+        sprintf(data,"AT*PCMD=%d,0,0,0,0,0\rAT*REF=%d,290717696\r",seq++,seq++);  //,aeq++); //seq++);
+	Serial.println("landingState command string: ");
+	Serial.println(data);
+}
+
+void takeOffState()
+{
+        //Serial.println("takeOffState, tell drone to take off");
+        //droneState = 1;
+        sprintf(data,"AT*PCMD=%d,0,0,0,0,0\rAT*REF=%d,290718208\r",seq++,seq++);  //,aeq++); //seq++);
+	Serial.println("takeOffState command string: ");
+	Serial.println(data);
+}
+
+void hoverState()
+{
+        //Serial.println("hoverState, hovring with no directions");
+        //droneState = 2;
+        sprintf(data,"AT*PCMD=%d,0,0,0,0,0\r",seq++);
+	Serial.println("hoverState command string: ");
+	Serial.println(data);
+//aeq++;
+}
+
+void emergencyLandState()
+{
+        //Serial.println("landingState send land command");
+        //droneState = 3;
+	//sprintf(data,"AT*REF=%d,290717696\r", seq++);
+        sprintf(data,"AT*PCMD=%d,0,0,0,0,0\rAT*REF=%d,290717696\r",seq++, seq++);  //,aeq++); //seq++);
+	Serial.println("emergencyLandState command string: ");
+	Serial.println(data);
+}
+
+void forwardState()
+{
+	//Serial.println("forwardState, moving forward");
+	//droneState = 4;
+
+	// Entered a 1 for the second arg in the command to enable reading other values for control
+	sprintf(data, "AT*PCMD=%d,1,0,%ld,0,0\r", seq++, forwardSpeed);
+	Serial.println("forward command string: ");
+	Serial.println(data);
+	//Serial.println("");
+	//Serial.print("forwardSpeedValue: ");
+	//Serial.println(forwardSpeed);
+        //sprintf(data,"AT*PCMD=%d,1,0,forwardSpeed,0,0\r",seq++);
+        //sprintf(data,"AT*PCMD=%d,0,0,forwardSpeed,0,0\r",seq++);
+}
+
+void reverseState()
+{
+	//Serial.println("rangeStop has been breached, reverse drone then pass state back to run time.");
+
+	if (reverseTime == 0)
+	{
+		reverseTime = millis();
+	}
+
+	if (millis() - reverseTime < 200)
+	{
+		sprintf(data, "AT*PCMD=%d,1,0,%ld,0,0\r", seq++, reverseSpeed);
+	}
+	else
+	{
+		reverseTime = 0;
+		reverseTrigger = 0;
+	}
+
+	Serial.println("Reverse command string, send for 200 ms: ");
+	Serial.println(data);
+}
+
+void keepConnection()
+{
+	//Serial.print("keepConnection() Function: droneState = ");
+	//Serial.println(droneState);
+
+	//if (millis() - keepConn > 300)
+	// Check for 30 milliseconds rather than 300 for keep connection,
+	// should enable smoother flight
+	if (millis() - keepConn > 30)
+	{
+		  keepConn = millis();
+
+		  switch(droneState)
+		  {
+			case LANDED:
+				landingState();
+				break;
+			case TAKEOFF:
+				takeOffState();
+				break;
+			case HOVERING:
+				hoverState();
+				break;
+			case EMERGENCYLAND:
+				emergencyLandState();
+				break;
+			case FORWARDFLIGHT:
+				forwardState();
+				break;
+			case REVERSE:
+				reverseState();
+				break;
+		  }
+	}
+}
+
+void sensorCompare()
+{
+	//Serial.println("sensorCompare function");
+
+	int i;
+	int leftRead = 0;
+	int rightRead = 0;
+	int verticalRead = 0;
+
+	for (i = 0; i < 3; i++)
+	{
+		if (leftRead < leftSensor[i])
+		{
+			leftRead = leftSensor[i];
+		}
+		if (rightRead < rightSensor[i])
+		{
+			rightRead = rightSensor[i];
+		}
+		if (verticalRead < verticalSensor[i])
+		{
+			verticalRead = verticalSensor[i];
+		}
+	}
+
+	leftRange = leftRead;
+	rightRange = rightRead;
+	verticalRange = verticalRead;
+
+	// DebugPrintFunction
+	//debugPrint();
+}
+
+void rangeCheck()
+{
+	//Serial.println("rangeCheck function");
+	if (rangeTimer < 3)
+	{
+		leftSensor[rangeTimer] = rangeClass.sensors[left]; //0];
+		rightSensor[rangeTimer] = rangeClass.sensors[right]; //1];
+		verticalSensor[rangeTimer] = rangeClass.sensors[vertical]; //2];
+		rangeTimer++;
+	}
+
+	//Serial.println(rangeTimer);
+
+	if (rangeTimer == 3)
+	{
+		sensorCompare();
+	}
+}
+
+int goForward()
+{
+	//debugPrint();
+
+	if (leftRange > rangeStop && rightRange > rangeStop)
+	{
+		//Serial.println("goForward returning a #1!");
+		return 1;
+	}
+
+	//Serial.println("goForward returning a 0!");
+	return 0;
+}
+
+void forwardSensorTest()
+{
+	if (time == 0)
+	{
+		initialState();
+	}
+
+	// populate range arrays and get range values
+	if (millis() - time > 100)
+	{
+		rangeClass.GetRanges();
+		rangeCheck();
+		time = millis();
+	}
+
+	if (rangeTimer >= 3)
+	{
+		rangeTimer = 0;
+
+		switch (droneState)
+		{
+			case LANDED:
+			{
+				if (millis() - flightTime > 5000)
+				{
+					droneState = TAKEOFF;
+					flightTime = millis();
+				}
+
+				break;
+			}
+			case TAKEOFF:
+			{
+				if (millis() - flightTime > 5000)
+				{
+					droneState = HOVERING;
+					flightTime = millis();
+				}
+
+				break;
+			}
+			case HOVERING:
+			{
+				//if (millis() - flightTime > 5000)
+				if (goForward())
+				{
+					droneState = FORWARDFLIGHT;
+					flightTime = millis();
+				}
+
+				break;
+			}
+			case FORWARDFLIGHT:
+			{
+				if (goForward())
+				{
+					//Serial.println("Flying forward!!!!");
+					droneState = FORWARDFLIGHT;
+				}
+				else
+				{
+					//Serial.println("rangeStop has been breached, STOP the drone!!!");
+
+					droneState = REVERSE;
+					reverseTrigger = 1;
+				}
+
+				break;
+			}
+			case REVERSE:
+			{
+				//Serial.println("Drone in REVERSE state");
+				//Serial.print("reverseTrigger: ");
+				//Serial.println(reverseTrigger);
+
+				if (reverseTrigger == 0)
+				{
+					droneState = HOVERING;
+				}
+
+				break;
+			}
+		}
+	}
+
+	if (millis() - totalTime > 20000)
+	{
+		droneState = EMERGENCYLAND;
+	}
+
+	keepConnection();
+
+}
+
+void debugPrint()
+{
+
+	// Range finder print Statments
+	Serial.print("RangeTimer; ");
+	Serial.print(rangeTimer);
+	Serial.print("; DroneState; ");
+	Serial.print(droneState);
+	Serial.print("; Left Sensor; ");
+	Serial.print(leftRange); //rangeClass.sensors[0]);
+	Serial.print("; Right Sensor; ");
+	Serial.print(rightRange); //rangeClass.sensors[1]);
+	Serial.print("; Vertical Sensor; ");
+	Serial.println(verticalRange); //rangeClass.sensors[2]);
+}
+
+void setup()
+{
+        Serial.begin(9600);
+        Serial.println("");
+        Serial.println("");
+        Serial.println("Initialize Connection");
+        
+	if (!testing)
+	{
+		WiFi.init();
+		Serial.println("Initialized");
+        
+		unsigned char *my_mac = zg_get_mac();
+		sprintf(yj_mac,"%.2X:%.2X:%.2X:%.2X:%.2X:%.2X",my_mac[0],my_mac[1],my_mac[2],my_mac[3],my_mac[4],my_mac[5]);
+		
+		Serial.print("MAC Address: ");
+		Serial.println(yj_mac);
+	}
+	else
+	{
+		Serial.println("Testing program");
+	}
+}
+
+void loop()
+{
+	if (!testing)
+	{
+		WiFi.run();
+	}
+	
+        // Function to begin forward flight test
+        forwardSensorTest();
+}
